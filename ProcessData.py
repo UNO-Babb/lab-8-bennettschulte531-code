da34879cd62eef421bf2483586b491e32bdbd0c9
bennettschulte531-code <bennettschulte531@gmail.com>
2025-11-01 22:00:05 +0000

    Done

--- ProcessData.py
+++ ProcessData.py
@@ -1,6 +1,6 @@
 #ProcessData.py
-#Name:
-#Date:
+#Name:Bennett Schulte
+#Date:11/1/2025
 #Assignment:
 
 import random
@@ -12,6 +12,50 @@ def main():
   outFile = open("StudentList.csv", 'w')
 
   #Process each line of the input file and output to the CSV file
+  header = inFile.readline()
+
+  for line in inFile:
+    parts = line.strip().split()
+
+    if len(parts) < 7:
+      continue
+
+    firstName = parts[0]
+    lastName = parts[1]
+    email = parts[2]
+    studentID = parts[3]
+    dob = parts[4]
+    year = parts[5]
+    major = parts[6]
+
+    firstInitial = firstName[0]
+    lastThree = studentID[-3:]
+    userID = firstInitial + lastName + lastThree
+
+    if len(lastName) < 5:
+      userID += "X"
+
+    majorShort = major[:3].upper()
+
+    yearLower = year.lower()
+    if "fresh" in yearLower:
+      yearShort = "FR"
+    elif "soph" in yearLower:
+      yearShort = "SO"
+    elif "jun" in yearLower:
+      yearShort = "JR"
+    elif "sen" in yearLower:
+      yearShort = "SR"
+    else:
+      yearShort = "NA"
+
+    majorYear = majorShort + "-" + yearShort
+  
+    outLine = f"{lastName},{firstName},{userID},{majorYear}\n"
+    outFile.write(outLine)
+
+  
+  
 
 
 
